6b5614f4d1ecfd5c7d3a563f50c6b71b72633032
Prem <pprakash312003@gmail.com>
2024-08-20 00:37:06 +0530

    class based component

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,24 @@ function App(){
 }
 
 
-function MyComponent() {
-  const [count, setCount] = useState(0);
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
 
-  const incrementCount = () => {
-    setCount(count + 1);
-  };
+  incrementCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  }
 
-  return (
-    <div>
-      <p>{count}</p>
-      <button onClick={incrementCount}>Increment</button>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <p>{this.state.count}</p>
+        <button onClick={this.incrementCount}>Increment</button>
+      </div>
+    );
+  }
 }
 
 export default App
