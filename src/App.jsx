3a875b46bd8a82c76c86477789806a040fb7b2a7
Prem <pprakash312003@gmail.com>
2024-08-20 14:41:18 +0530

    hook into functional component in lifecycle event

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 
-function App(){
-  return (
-    <>
-     <MyComponent />
-    </>
-  )
+
+
+function App() {
+const [render, setRender] = useState(true);
+  useEffect(()=>{
+  setTimeout(() =>{
+    setRender(false);
+
+  },10000)
+},[]);
+
+return (
+  <>
+   {render ? <MyComponent /> : <div></div>}
+  </>
+)
 }
 
+function MyComponent() {
+  useEffect(() => {
+    console.error("component mounted");
 
-class MyComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
-  }
+    return ()  => {
+      console.log("componment unmounted");
+    };
+  }, []);
 
-  incrementCount = () => {
-    this.setState({ count: this.state.count + 1 });
-  }
-
-  render() {
-    return (
-      <div>
-        <p>{this.state.count}</p>
-        <button onClick={this.incrementCount}>Increment</button>
-      </div>
-    );
-  }
+  return <div>
+    From inside my component
+  </div>
 }
 
 export default App
