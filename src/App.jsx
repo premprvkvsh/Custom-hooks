c7d89dc848cf1eaf5619fc782c7b277325d5118c
Prem <pprakash312003@gmail.com>
2024-08-22 03:05:31 +0530

    useInterval

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,26 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import { useIsOnline } from './hooks/useIsOnline';
-
-// function useTodos(n) {
-//   const [todos, setTodos] = useState([])
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     const value = setInterval(() => {
-//       axios.get("https://sum-server.100xdevs.com/todos")
-//         .then(res => {
-//           setTodos(res.data.todos);
-//           setLoading(false);
-//         })
-//     }, n * 1000)
-   
-//     axios.get("https://sum-server.100xdevs.com/todos")
-//       .then(res => {
-//         setTodos(res.data.todos);
-//         setLoading(false);
-//       })
-
-//     return () => {
-//       clearInterval(value)
-//     }
-//   }, [n])
-
-//   return {todos, loading};
-// }
-
-// function App() {
-//   const {todos, loading} = useTodos(10);
-
-//   if (loading) {
-//     return <div> loading... </div>
-//   }
-
-//   return (
-//     <>
-//       {todos.map(todo => <Track todo={todo} />)}
-//     </>
-//   )
-// }
-
-// function Track({ todo }) {
-//   return <div>
-//     {todo.title}
-//     <br />
-//     {todo.description}
-//   </div>
-// }
+import { useEffect, useState } from 'react';
 
 
 
-// function App() {
-//   const isOnline = useIsOnline(5);
-
-//   return (
-//     <>
-//       {isOnline ? "You are online yay!" : "You are not online"}
-//     </>
-//   )
-// }
-
-
-const useMousePointer = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-
-  const handleMouseMove = (e) => {
-    setPosition({ x: e.clientX, y: e.clientY });
-  };
-
+function useInterval(fn, timeout){
   useEffect(() => {
-    window.addEventListener('mousemove', handleMouseMove);
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
-
-  return position;
-};
-
+    setInterval(() => {
+      fn()
+    }, timeout);
+  })
+}
 function App() {
-  const mousePointer = useMousePointer();
+  const [count, setCount] = useState(0);
+
+  useInterval(() => {
+    setCount(c => c + 1);
+  }, 1000)
 
   return (
     <>
-      Your mouse position is {mousePointer.x} {mousePointer.y}
+      Timer is at {count}
     </>
   )
 }
 
-
 export default App
-
-
