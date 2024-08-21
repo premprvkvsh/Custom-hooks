6ccb3d7df6239d31021d10b302ff029fc8452205
Prem <pprakash312003@gmail.com>
2024-08-22 02:11:51 +0530

    Auto refreshing hook  and Cleaning the hook to include a loading parameter

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-
-function useTodos(){
+function useTodos(n) {
   const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    const value = setInterval(() => {
+      axios.get("https://sum-server.100xdevs.com/todos")
+        .then(res => {
+          setTodos(res.data.todos);
+          setLoading(false);
+        })
+    }, n * 1000)
+  
     axios.get("https://sum-server.100xdevs.com/todos")
       .then(res => {
         setTodos(res.data.todos);
+        setLoading(false);
       })
-      .catch(err => {
-        console.error("Error fetching data", err);
-      });
-  }, [])
-  
-  return todos;
+
+    return () => {
+      clearInterval(value)
+    }
+  }, [n])
+
+  return {todos, loading};
 }
 
 function App() {
-  const todos = useTodos();
+  const {todos, loading} = useTodos(10);
+
+  if (loading) {
+    return <div> loading... </div>
+  }
 
   return (
     <>
