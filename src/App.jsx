16bc50389123ef6324972f6481c086700eb6cae7
Prem <pprakash312003@gmail.com>
2024-08-22 02:22:57 +0530

    useMousePointer

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,97 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import { useIsOnline } from './hooks/useIsOnline';
 
-function useTodos(n) {
-  const [todos, setTodos] = useState([])
-  const [loading, setLoading] = useState(true);
+// function useTodos(n) {
+//   const [todos, setTodos] = useState([])
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const value = setInterval(() => {
+//       axios.get("https://sum-server.100xdevs.com/todos")
+//         .then(res => {
+//           setTodos(res.data.todos);
+//           setLoading(false);
+//         })
+//     }, n * 1000)
+   
+//     axios.get("https://sum-server.100xdevs.com/todos")
+//       .then(res => {
+//         setTodos(res.data.todos);
+//         setLoading(false);
+//       })
+
+//     return () => {
+//       clearInterval(value)
+//     }
+//   }, [n])
+
+//   return {todos, loading};
+// }
+
+// function App() {
+//   const {todos, loading} = useTodos(10);
+
+//   if (loading) {
+//     return <div> loading... </div>
+//   }
+
+//   return (
+//     <>
+//       {todos.map(todo => <Track todo={todo} />)}
+//     </>
+//   )
+// }
+
+// function Track({ todo }) {
+//   return <div>
+//     {todo.title}
+//     <br />
+//     {todo.description}
+//   </div>
+// }
+
+
+
+// function App() {
+//   const isOnline = useIsOnline(5);
+
+//   return (
+//     <>
+//       {isOnline ? "You are online yay!" : "You are not online"}
+//     </>
+//   )
+// }
+
+
+const useMousePointer = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
 
   useEffect(() => {
-    const value = setInterval(() => {
-      axios.get("https://sum-server.100xdevs.com/todos")
-        .then(res => {
-          setTodos(res.data.todos);
-          setLoading(false);
-        })
-    }, n * 1000)
-  
-    axios.get("https://sum-server.100xdevs.com/todos")
-      .then(res => {
-        setTodos(res.data.todos);
-        setLoading(false);
-      })
-
+    window.addEventListener('mousemove', handleMouseMove);
     return () => {
-      clearInterval(value)
-    }
-  }, [n])
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
 
-  return {todos, loading};
-}
+  return position;
+};
 
 function App() {
-  const {todos, loading} = useTodos(10);
-
-  if (loading) {
-    return <div> loading... </div>
-  }
+  const mousePointer = useMousePointer();
 
   return (
     <>
-      {todos.map(todo => <Track todo={todo} />)}
+      Your mouse position is {mousePointer.x} {mousePointer.y}
     </>
   )
 }
 
-function Track({ todo }) {
-  return <div>
-    {todo.title}
-    <br />
-    {todo.description}
-  </div>
-}
 
 export default App
+
+
---
+++ src/hooks/useIsOnline.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+export function useIsOnline() {
+    const [isOnline, setIsOnline] = useState(window.navigator.onLine);
+  
+    useEffect(() => {
+      window.addEventListener('online', () => setIsOnline(true));
+      window.addEventListener('offline', () => setIsOnline(false));
+    }, [])
+  
+    return isOnline;
+  }
